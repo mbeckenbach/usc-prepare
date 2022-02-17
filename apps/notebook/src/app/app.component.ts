fd9656f3f6f85e55d9f22a748abbd0b4079c9a08
Max Beckenbach <max@bebamail.de>
2022-02-17 14:20:42 +0100

    3-adding-a-backend: Adds a nestjs backend and connects apps

--- apps/notebook/src/app/app.component.ts
+++ apps/notebook/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'nb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
+  styleUrls: [ './app.component.sass' ],
 })
 export class AppComponent {
   title = 'notebook';
+
+  constructor(private http: HttpClient) {
+    this.http.get(`${environment.backendHost}/api`).subscribe(console.log);
+  }
 }
--- apps/taskboard/src/app/app.module.ts
+++ apps/taskboard/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
